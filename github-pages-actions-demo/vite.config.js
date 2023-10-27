c0943d2aaca78a9070bae20895144d367c688868
Osman Araujo <osmanaraujo979@gmail.com>
2023-10-27 10:50:32 -0600

    Subiendo Archivo

---
+++ github-pages-actions-demo/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import * as glob from 'glob';
+import path, { resolve } from 'node:path';
+import {ViteMinifyPlugin}from 'vite-plugin-minify'
+import htmlPurgue from 'vite-plugin-purgecss'; 
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                [...glob.sync('./!(dist)/*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ]),
+                ...glob.sync('./*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ])]
+            ),
+        },
+    },
+    plugins:[
+        htmlPurgue({}),
+        ViteMinifyPlugin({}),
+    ]
+})
